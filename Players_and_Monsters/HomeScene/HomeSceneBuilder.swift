7e7e6dde640c7f578a55a8563053dcec0f866fcc
AnnaVertikova <ann.vertikova@yandex.ru>
2023-10-07 16:28:22 +0700

    some cleaning

--- Players_and_Monsters/HomeScene/HomeSceneBuilder.swift
+++
@@ -1,8 +0,0 @@
-//
-//  HomeSceneBuilder.swift
-//  Players_and_Monsters
-//
-//  Created by Анна Вертикова on 06.10.2023.
-//
-
-import Foundation
--- Players_and_Monsters/Players_and_Monsters/AppDelegate.swift
+++ Players_and_Monsters/Players_and_Monsters/AppDelegate.swift
@@ -2,7 +2,7 @@
 //  AppDelegate.swift
 //  Players_and_Monsters
 //
-//  Created by Анна Вертикова on 27.09.2023.
+//  Created by Анна Вертикова on 07.10.2023.
 //
 
 import UIKit
